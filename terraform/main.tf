e24445925670145d3ff6b7f8e670d5b79126e0ed
fakhrul <tgfakhrul@gmail.com>
2026-01-07 14:35:01 +0800

    Add Security Group module in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,3 +54,56 @@ module "vpc" {
     Name = var.nat_gateway_name
   }
 }
+
+module "security-group-public" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+
+  name = "devops-public-sg"
+  description = "Web Server Security Group"
+  vpc_id = module.vpc.vpc_id
+
+# Ingress Rules
+  ingress_with_cidr_blocks = [
+    # HTTP -Allow From Everywhere
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "HTTP from everywhere"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    # Prometheus Node Exporter
+    {
+      from_port   = 9100
+      to_port     = 9100
+      protocol    = "tcp"
+      description = "Prometheus Node Exporter from Monitoring Server"
+      cidr_blocks = "0.0.0.0/0" #need fix Allow from Monitoring Server IP
+    },
+    # SSH from VPC subnet only
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH from VPC CIDR"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+  # Allow All Outbound Traffic
+  egress_rules = ["all-all"]
+}
+
+module "security-group-private" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+
+  name        = "devops-private-sg"
+  description = "Ansible Controller & Monitoring Server Security Group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
+  ingress_rules       = ["ssh-tcp"]
+  egress_rules        = ["all-all"]
+}
+
